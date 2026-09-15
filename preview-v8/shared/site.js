83cdbd8fe9ead18dfcec84c1607658c6398b3211
Carlitos <carlos.valdes.morales@gmail.com>
2026-09-15 00:49:19 -0300

    feat(mobile): load PWA app layer on every public CUDO view

--- preview-v8/shared/site.js
+++ preview-v8/shared/site.js
@@ -28,6 +28,7 @@
     return response;
   };
 
+  document.write('<script src="'+base+'pwa.js"><\/script>');
   document.write('<script src="'+base+'presentation-guard.js"><\/script>');
   document.write('<script src="'+base+'site-runtime-v8.js"><\/script>');
 })();
